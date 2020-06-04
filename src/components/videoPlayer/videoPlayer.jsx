8d84025126da4cd6666acf502107b052075e7417
gryphbecrazeh <gryphbecrazeh@gmail.com>
2020-06-04 15:12:52 -0400

    added some video controls

--- src/components/videoPlayer/videoPlayer.jsx
+++ src/components/videoPlayer/videoPlayer.jsx
@@ -15,7 +15,39 @@ let VideoPlayer = (props) => {
 			}
 		}
 	};
-	let renderNextEpisodeLink = () => {
+	let handleTimeUpdate = () => {
+		let player = document.querySelector("#video-player");
+		let video = player.querySelector("video");
+		let orangeBar = player.querySelector(".orange-bar");
+		let orangeJuice = orangeBar.querySelector(".orange-juice");
+		let juicePos = video.currentTime / video.duration;
+		let progress = player.querySelector("#progress");
+		progress.value = juicePos * 100;
+		orangeJuice.style.width = juicePos * 100 + "%";
+
+		showNextEpisode();
+	};
+	let handlePlayPause = () => {
+		let player = document.querySelector("#video-player");
+		let video = player.querySelector("video");
+		return video.paused ? video.play() : video.pause();
+	};
+	let handleFullScreen = () => {
+		let player = document.querySelector("#video-player");
+		let fullscreenStatus = document.fullscreen;
+
+		if (fullscreenStatus) {
+			return document.exitFullscreen();
+		}
+		return player.requestFullscreen();
+	};
+	let handleProgressSelect = (e) => {
+		let player = document.querySelector("#video-player");
+		let video = player.querySelector("video");
+		video.currentTime = (e.target.value / 100) * video.duration;
+		console.log(e.target.value);
+	};
+	let NextEpisodeLink = () => {
 		if (nextEpisode) {
 			return (
 				<span>
@@ -32,18 +64,38 @@ let VideoPlayer = (props) => {
 	return (
 		<div id="video-player">
 			<video
-				controls
+				// controls
 				autoplay
 				ref={videoRef}
 				width="100%"
 				height="auto"
-				onTimeUpdate={showNextEpisode}
+				onTimeUpdate={handleTimeUpdate}
 				onLoadedMetadata={() => videoRef.current.play()}
 				onEnded={changeEpisode}
 			>
 				<source src={`${episode.video}`}></source>
 			</video>
-			{renderNextEpisodeLink()}
+			<NextEpisodeLink />
+			<div className="vp-controls">
+				<input
+					type="range"
+					id="progress"
+					min="0"
+					max="100"
+					onClick={handleProgressSelect}
+				/>
+				<div className="orange-bar">
+					<div className="orange-juice"></div>
+				</div>
+				<div className="buttons">
+					<button id="play-pause" onClick={handlePlayPause}>
+						P
+					</button>
+					<button id="fullscreen" onClick={handleFullScreen}>
+						F
+					</button>
+				</div>
+			</div>
 		</div>
 	);
 };
